72c737cc4040ef6c1ba2c086ea6d4194dc5ce73c
Sree Sushanth B V <bvsreesushanth29802@gmail.com>
2026-02-06 10:38:31 +0530

    Update auth.js

--- money-manager-backend/routes/auth.js
+++ money-manager-backend/routes/auth.js
@@ -1,17 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-
-// router.post("/login", (req, res) => {
-//   const { email, password } = req.body;
-
-//   if (email === "admin@gmail.com" && password === "admin123") {
-//     return res.json({ success: true });
-//   }
-
-//   res.status(401).json({ message: "Invalid credentials" });
-// });
-
-// module.exports = router;
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
