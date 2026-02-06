5e39f3ea98a8c045209c1e167f114a55fe5e9b81
Sree Sushanth B V <bvsreesushanth29802@gmail.com>
2026-02-06 10:38:48 +0530

    Update profile.js

--- money-manager-backend/routes/profile.js
+++ money-manager-backend/routes/profile.js
@@ -1,34 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const User = require("../models/User");
-
-// // GET PROFILE
-// router.get("/:id", async (req, res) => {
-//   try {
-//     const user = await User.findById(req.params.id).select("-password");
-//     if (!user) return res.status(404).json({ message: "User not found" });
-//     res.json(user);
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// });
-
-// // UPDATE PROFILE
-// router.put("/:id", async (req, res) => {
-//   try {
-//     const updated = await User.findByIdAndUpdate(
-//       req.params.id,
-//       req.body,
-//       { new: true }
-//     ).select("-password");
-
-//     res.json(updated);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// });
-
-// module.exports = router;
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
